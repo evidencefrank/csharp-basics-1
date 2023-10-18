649829c353f29b572fbcaed1aab36af753d590f8
evidencefrank <evidencefrank@outlook.com>
2023-10-18 09:23:45 +0200

    taking more challenges

---
+++ TestProject11/Program.cs
@@ -0,0 +1,17 @@
+﻿int heroPoints = 10;
+int monsterPoints = 10;
+Random attack = new Random();
+int loss = 0;
+
+do{
+    loss = attack.Next(1, 11);
+    monsterPoints -= loss;
+    Console.WriteLine($"Monster was damaged and lost {loss} health and now has {monsterPoints} health.");
+    if(monsterPoints <= 0) continue;
+    loss = attack.Next(1, 11);
+    heroPoints -= loss;
+    Console.WriteLine($"Hero was damaged and lost {loss} health and now has {heroPoints} health.");
+
+}while(heroPoints > 0 &&  monsterPoints > 0);
+
+Console.WriteLine(heroPoints > monsterPoints ? "Hero wins!" : "Monster wins!");
